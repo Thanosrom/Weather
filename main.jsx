d82b6a3aef4db98c70ed9ead28016861fdbf7ca2
Thanosrom <85241909+Thanosrom@users.noreply.github.com>
2024-03-26 10:46:34 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './View/Pages/App'
+import Home from './View/Pages/Home'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
